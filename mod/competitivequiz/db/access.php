3f3887a72a93f145375fcecfbcd280d62759b8e1
bjoernkarthein <bjoern.karthein@t-online.de>
2021-06-06 12:33:51 +0200

    bare minimum setup for plugin to be recognized by moodle

---
+++ mod/competitivequiz/db/access.php
@@ -0,0 +1,26 @@
+<?php
+$capabilities = array(
+
+    'mod/competitivequiz:addinstance' => array(
+        'riskbitmask' => RISK_XSS,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/course:manageactivities'
+    ),
+
+    'mod/competitivequiz:view' => array(
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetypes' => array(
+            'guest' => CAP_ALLOW,
+            'student' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        )
+    )
+);
